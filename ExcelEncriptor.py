809a5d524b3911118c70e2590988a04204620890
QinxiuW <48995165+QinxiuW@users.noreply.github.com>
2023-04-17 23:27:10 +0200

    Create ExcelEncriptor.py

---
+++ ExcelEncriptor.py
@@ -0,0 +1,87 @@
+import os
+import glob
+import win32com.client as win32
+import PySimpleGUI as sg
+
+# Función para encriptar los archivos Excel en un directorio
+def encriptar_excel(directorio, password):
+    # Obtener la lista de archivos Excel en el directorio
+    archivos_excel = glob.glob(os.path.join(directorio, '*.xlsx'))
+
+    # Crear una instancia de Excel
+    excel = win32.gencache.EnsureDispatch('Excel.Application')
+
+    # Iterar sobre cada archivo Excel y protegerlo con contraseña
+    for archivo in archivos_excel:
+        workbook = excel.Workbooks.Open(archivo, Password=password)
+        workbook.Save()
+        workbook.Close()
+
+    # Cerrar Excel
+    excel.Quit()
+
+# Función para abrir todos los archivos Excel en un directorio
+def abrir_excel(directorio, password):
+    # Obtener la lista de archivos Excel en el directorio
+    archivos_excel = glob.glob(os.path.join(directorio, '*.xlsx'))
+
+    # Crear una instancia de Excel
+    excel = win32.gencache.EnsureDispatch('Excel.Application')
+
+    # Iterar sobre cada archivo Excel y abrirlo
+    for archivo in archivos_excel:
+        print(archivo)
+        workbook = excel.Workbooks.Open(archivo,Password=password)
+        excel.Visible = True
+
+    # Cerrar Excel
+    # excel.Quit()
+
+# Definir la interfaz de usuario
+layout = [
+    [sg.Text('Encriptar archivos Excel')],
+    [sg.FolderBrowse(key='directorio'), sg.Text('Directorio:', size=(20, 1))],
+    [sg.Text('Contraseña:', size=(10, 1)), sg.InputText(key='password')],
+    [sg.Submit(button_text='Encriptar')],
+    [sg.Text('')],
+    [sg.Submit(button_text='Abrir')],
+]
+
+# Crear la ventana de la aplicación
+window = sg.Window('Mi aplicación', layout, size=(300, 300))
+
+# Bucle principal de la aplicación
+while True:
+    event, values = window.Read()
+
+    if event == 'Encriptar':
+        directorio = values['directorio']
+        password = values['password']
+        
+        # Verificar si el directorio existe y contiene archivos .xlsx
+        if not os.path.exists(directorio):
+            sg.popup('El directorio especificado no existe.')
+        elif not glob.glob(os.path.join(directorio, '*.xlsx')):
+            sg.popup('El directorio no contiene archivos Excel (.xlsx).')
+        elif not password:
+            sg.popup('La contraseña no puede estar vacía.') 
+        else:
+            encriptar_excel(directorio, password)
+            sg.popup('Los archivos han sido encriptados con éxito.')
+
+    if event == 'Abrir':
+        directorio = values['directorio']
+        password = values['password']
+        
+        # Verificar si el directorio existe y contiene archivos .xlsx
+        if not os.path.exists(directorio):
+            sg.popup('El directorio especificado no existe.')
+        elif not glob.glob(os.path.join(directorio, '*.xlsx')):
+            sg.popup('El directorio no contiene archivos Excel (.xlsx).')
+        elif not password:
+            sg.popup('La contraseña no puede estar vacía.') 
+        else:
+            abrir_excel(directorio,password)
+            sg.popup('Los archivos han sido abiertos con éxito.')
+# Cerrar la ventana de la aplicación
+window.Close()
